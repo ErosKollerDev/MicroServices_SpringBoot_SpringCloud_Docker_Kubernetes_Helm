9abd7f31986fe4f5d91ebff23084e955ee06db93
ErosKollerDev <eroskoller.dev@gmail.com>
2025-09-17 15:19:57 -0300

    Config Server

---
+++ 07_section/cards/src/main/java/com/eazybytes/cards/entity/Cards.java
@@ -0,0 +1,33 @@
+package com.eazybytes.cards.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+
+
+@Getter
+@Setter
+@Entity
+public class Cards extends BaseEntity{
+
+    @Id
+    @Getter
+    @Setter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cardId;
+
+
+
+    private String mobileNumber;
+    private String cardNumber;
+    private String cardType;
+    private Integer totalLimit;
+    private Integer amountUsed;
+    private Integer availableAmount;
+
+
+}
