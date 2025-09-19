d222fd28432546edad5470600c8d8762b0f62163
ErosKollerDev <eroskoller.dev@gmail.com>
2025-09-19 09:13:46 -0300

    Gateway

---
+++ 09_gateway/cards/src/main/java/com/eazybytes/cards/repository/CardsRepository.java
@@ -0,0 +1,20 @@
+package com.eazybytes.cards.repository;
+
+import com.eazybytes.cards.dto.CardsDto;
+import com.eazybytes.cards.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+
+
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+}
